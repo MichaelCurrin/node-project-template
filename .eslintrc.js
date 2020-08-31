7c28d1e9ebf7d6e3b71a3e9ce69d3ee73ba7e7f9
Michael Currin <michael.currin@gmail.com>
2020-08-31 12:49:00 +0200

    chore: Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: [
+      2,
+      'always'
+    ]
+  }
+};
